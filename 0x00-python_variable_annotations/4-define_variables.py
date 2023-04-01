85ea3e1992d6f1178e0892b1eaa4f221c716ea6c
root <ajaomalik2018@gmail.com>
2023-04-01 04:58:29 +0000

    added

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
-    a: int
-    a = 1
-    pi: float
-    pi = 3.14
-    i_understand_annotations: bool
-    i_understand_annotations = True
-    school: str
-    school = "Holberton"
+a: int
+a = 1
+pi: float
+pi = 3.14
+i_understand_annotations: bool
+i_understand_annotations = True
+school: str
+school = "Holberton"
