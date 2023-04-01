0b4894b8e7f1c9a562ecc46905642053a77ffcc1
root <ajaomalik2018@gmail.com>
2023-04-01 04:56:38 +0000

    added

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def to_str(n: float):
+    """converts a float to str"""
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    a: int
+    a = 1
+    pi: float
+    pi = 3.14
+    i_understand_annotations: bool
+    i_understand_annotations = True
+    school: str
+    school = "Holberton"
