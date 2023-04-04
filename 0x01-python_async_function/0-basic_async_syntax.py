8330ee865129b5a022370af0185540308eae453e
root <ajaomalik2018@gmail.com>
2023-04-04 05:10:38 +0000

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import asyncio
+import time
 import random
 
 
 async def wait_random(max_delay=10) -> float:
     random_time = random.randint(max_delay + 1)
-    await asyncio.sleep(random_time)
+    await time.sleep(random_time)
     return random_time
